ac2dd55171ae6bf465572d9f0c729254bb549ae5
Frey <freylyons@hotmail.co.uk>
2025-02-23 22:13:31 +0000

    Created a todo for refactor and some code comments

---
+++ TODO.md
@@ -0,0 +1,12 @@
+# TODO
+
+## Macro
+
+    1)  Refactor the common/default.nix into a series of modules by purpose. Sort the modules into directories by types (services, programs, etc.)
+    2)  Use default.nix purely to load modules into the default configuration
+    3)  alter the configuration.nix files to use the default.nix and enable/disable modules on the system as necessary
+    4)  simplify the flake.nix file to specify the configuration for each host, rather than specifying/importing the modules there.
+    5)  examine the possibility of creating a neovim configuration. 
+
+## Micro
+    
--- common/home-manager.nix
+++ common/home-manager.nix
@@ -11,7 +11,7 @@
     home.stateVersion = "23.11";
 
     #
-    # program settings
+    # --- program settings---
     #
 
     programs.git = {
@@ -20,8 +20,37 @@
       userEmail = "freylyons@hotmail.co.uk";
     };
 
+    # --- set up firefox configuration ---
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.firefox.profiles
+    # firefox uses profiles to store configuration information (bookmarks, passwords, and extensions), so recreating my firefox environment declaratively here will be done through that mechanism
+
+
+    # --- set up dolphin configuration ---
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-home.file._name_.text
+    # dolphin is configured using the dolphinrc configuration file in ~/.config/dolphinrc. Use `home.file.<name>.text` to write the file output (replace <name> with the path in the home directory). Using this method might create issues for changing settings manually, as it is stored in a read only location according to chatgpt.
+
+    # also consider using kwriteconfig6 https://nix-community.github.io/home-manager/options.xhtml#opt-qt.kde.settings
+    # this passes options to a built in function to alter settings by (seemingly) altering existing config files. This might be a better option long term.
+    #     home.file.".config/dolphinrc".text = ''
+    #       [Terminal]
+    #       ShowTerminal=true
+    #       TerminalPosition=2
     #
-    # desktop settings
+    #       [KFileDialog Settings]
+    #       Show hidden files=true
+    #     '';
+    #     home.file.".local/share/kfileplaces/bookmarks.xml".text = ''
+    #       <?xml version="1.0" encoding="UTF-8"?>
+    #       <bookmarks>
+    #         <bookmark href="file:///home/frey/files/documents" icon="folder-documents" id="Documents" />
+    #         <bookmark href="file:///home/frey/downloads" icon="folder-download" id="Downloads" />
+    #         <!-- Add more bookmarks as needed -->
+    #       </bookmarks>
+    #     '';
+
+
+    #
+    # --- desktop settings ---
     #
 
     programs.plasma = {
