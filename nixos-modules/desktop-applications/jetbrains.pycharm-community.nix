239fe8d00e628671a58bf61c72dc499afbe393f7
Frey <freylyons@hotmail.co.uk>
2025-03-08 16:52:36 +0000

    refactored the machine-level desktop applications modules to only build machine level applications when enabled

--- nixos-modules/desktop-applications/jetbrains.pycharm-community.nix
+++ nixos-modules/desktop-applications/jetbrains.pycharm-community.nix
@@ -1,26 +1,16 @@
 { pkgs, lib, config, ... } :
 let
-  optionName = "pycharm";
-  package = "jetbrains.pycharm-community";
+  package = "pycharm";
+  packageName = "jetbrains.pycharm-community";
 in
 {
   options = {
-    ${optionName} = {
-      enable = lib.mkEnableOption "enables the ${package} configuration on the system";
-      packageUser = lib.mkOption {
-        type = lib.types.nullOr lib.types.str;
-        default = null;
-        description = "If set, installs ${package} for the specified user instead of system-wide.";
-      };
-    };
+    ${package}.enable = lib.mkEnableOption "enables the ${package} configuration on the system";
   };
 
-  config = lib.mkMerge [
-    (lib.mkIf (config.${optionName}.enable && config.${optionName}.packageUser == null) {
-      environment.systemPackages = [ pkgs.${package} ];
-    })
-    (lib.mkIf (config.${optionName}.enable && config.${optionName}.packageUser != null) {
-      users.users.${config.${optionName}.packageUser}.packages = [ pkgs.${package} ];
-    })
-  ];
+  config = lib.mkIf config.${package}.enable {
+      environment.systemPackages = [ pkgs.${packageName} ];
+    };
 }
+
+
