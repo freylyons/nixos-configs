b653dfafd4a90fef001bfff3cd4f9172eac4d94d
Frey <freylyons@hotmail.co.uk>
2025-05-30 19:56:59 +0100

    remove references to home manager from the flake

--- flake.nix
+++ flake.nix
@@ -4,22 +4,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs"; # Ensure consistency with nixpkgs
-    };
-    plasma-manager = {
-      url = "github:nix-community/plasma-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
     nvf = {
       url = "github:notashelf/nvf";
       inputs.nixpkgs.follows = "nixpkgs"; # Ensure consistency with nixpkgs
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, plasma-manager, nvf, ... } @inputs:
+  outputs = { self, nixpkgs, nvf, ... } @inputs:
   let
     # system
     default_system = "x86_64-linux";
@@ -32,22 +23,12 @@
   in
   {
     nixosConfigurations =
-    let
-      home_manager_attrs = user_config: {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.frey = import configPath.user.frey; # import user "frey"'s configuration
-
-        home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
-      };
-    in
     {
       laptop = nixpkgs.lib.nixosSystem {
         system = default_system;
         specialArgs = { inherit inputs; }; # Pass inputs to configs
         modules = [
           ./hosts/laptop/configuration.nix  # Laptop-specific config
-          home-manager.nixosModules.home-manager home_manager_attrs # home manager module (allows home-manager to be built with system)
           nvf.nixosModules.default
         ];
       };
@@ -57,7 +38,6 @@
         specialArgs = { inherit inputs; }; # Pass inputs to configs
         modules = [
           ./hosts/desktop/configuration.nix
-          home-manager.nixosModules.home-manager home_manager_attrs # home manager module (allows home-manager to be built with system)
           nvf.nixosModules.default
         ];
       };
@@ -68,14 +48,6 @@
           ./hosts/pone-drone-hypervisor/configuration.nix
           nvf.nixosModules.default
         ];
-      };
-    };
-
-    # standalone home-manager configuration
-    homeConfigurations = {
-      frey = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [ configPath.user.frey ];
       };
     };
   };
